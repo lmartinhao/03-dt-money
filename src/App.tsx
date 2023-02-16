4edaae209b053a7fe2501eecdfaf17f5712ed793
lola martinhao <lola.martinhao@gmail.com>
2023-02-16 18:44:49 -0300

    adding global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+import { ThemeProvider } from "styled-components"
+import { defaultTheme } from "./styles/themes/default"
+
 export function App() {
 
   return (
-    <h1>Hello!</h1>
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Hello!</h1>
+    </ThemeProvider>
+
   )
 }
