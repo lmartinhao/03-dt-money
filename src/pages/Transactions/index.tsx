0d68a127df042b63ca85cd70b26b99ba828c8ba9
lola martinhao <lola.martinhao@gmail.com>
2023-02-22 15:26:03 -0300

    transactions search form added

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
+import { SearchForm } from './components/SearchForm'
 
 import {
   PriceHighlight,
@@ -13,6 +14,8 @@ export function Transactions() {
       <Header />
       <Summary />
       <TransactionsContainer>
+        <SearchForm />
+
         <TransactionsTable>
           <tbody>
             <tr>
